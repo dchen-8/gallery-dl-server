71ab2b34952a2bf7163caa1742249d18629170fd
dchen-8 <david.chen82688@gmail.com>
2020-08-16 13:41:30 -0700

    Added create_zip url path to generate cbz

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:alpine
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . /usr/src/app
+
+EXPOSE 8080
+
+VOLUME ["/usr/src/app/gallery-dl"]
+
+CMD [ "python3", "./gallery-dl-server.py" ]
--- gallery-dl-server.py
+++ gallery-dl-server.py
@@ -1,12 +1,17 @@
 # /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
+import os
 from bottle import route, run, Bottle, request
 import gallery_dl
+from zipfile import ZipFile
 
 app = Bottle()
 
 DEFAULT_HOST = '0.0.0.0'
 DEFAULT_PORT = 8080
+
+GALLERY_PATH = './gallery-dl/'
+ZIP_SUFFIX = 'cbz'
 
 @app.route('/')
 def gallery_main():
@@ -24,14 +29,32 @@ def gallery_post():
 
     call_gallery_dl(url)
 
-def call_gallery_dl(url):
-    global_config = gallery_dl.config
-    print(global_config._config)
-    global_config.set(url, 'dest', '/gallery-dl',)
 
+def call_gallery_dl(url):
     download_job = gallery_dl.job.DownloadJob
     download_job(url).run()
     print('Finished downloading!')
+
+@app.route('/gallery-dl/create_zip', method='GET')
+def find_directories_and_zip():
+    top_dir = os.listdir(GALLERY_PATH)
+    for each_dir in top_dir:
+        each_dir_path = os.path.join(GALLERY_PATH, each_dir)
+        for root_path, dirct, files in os.walk(each_dir_path):
+
+            photos_in_directory = [x for x in files if x.rsplit('.', 1)[1] in ('jpg', 'png')]
+            if not photos_in_directory:
+                print('No photos in directory: ' + root_path)
+                continue
+            
+            zip_path, zip_file = root_path.rsplit('/', 1)
+            zip_file_name = zip_file + '.' + ZIP_SUFFIX
+            zip_file_path = os.path.join(zip_path, zip_file_name)
+            with ZipFile(zip_file_path, 'w') as myzip:
+                for each_photo in photos_in_directory:
+                    each_photo_path = os.path.join(root_path, each_photo)
+                    myzip.write(each_photo_path)
+            print('Finished creating zip for: ' + root_path)
 
 
 app.run(host=DEFAULT_HOST, port=DEFAULT_PORT, debug=True)
