d04b46ebd3ab43a0a3b13e23ace2af330bee7964
dchen-8 <david.chen82688@gmail.com>
2020-08-16 14:51:29 -0700

    Added index.html page and checker for zipped files

--- gallery-dl-server.py
+++ gallery-dl-server.py
@@ -1,7 +1,7 @@
 # /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
 import os
-from bottle import route, run, Bottle, request
+from bottle import route, run, Bottle, request, static_file
 import gallery_dl
 from zipfile import ZipFile
 
@@ -15,7 +15,7 @@ ZIP_SUFFIX = 'cbz'
 
 @app.route('/')
 def gallery_main():
-    return {'Main Page'}
+    return static_file('index.html', root='./')
 
 
 @app.route('/gallery-dl', method='POST')
@@ -28,6 +28,8 @@ def gallery_post():
     print('Downloading: ' + url)
 
     call_gallery_dl(url)
+
+    return {"successfully_downloaded": True}
 
 
 def call_gallery_dl(url):
@@ -42,6 +44,7 @@ def find_directories_and_zip():
         each_dir_path = os.path.join(GALLERY_PATH, each_dir)
         for root_path, dirct, files in os.walk(each_dir_path):
 
+            # Check if there are photos in the files, if not skip directory
             photos_in_directory = [x for x in files if x.rsplit('.', 1)[1] in ('jpg', 'png')]
             if not photos_in_directory:
                 print('No photos in directory: ' + root_path)
@@ -50,11 +53,20 @@ def find_directories_and_zip():
             zip_path, zip_file = root_path.rsplit('/', 1)
             zip_file_name = zip_file + '.' + ZIP_SUFFIX
             zip_file_path = os.path.join(zip_path, zip_file_name)
+
+            # Check if zip file has already been created and skip if already created
+            # TODO: Allow ability to ignore check and re-zip folders.
+            if zip_file_name in os.listdir(zip_path):
+                print('Files have already been zipped.')
+                print('Skipping')
+                continue
+
             with ZipFile(zip_file_path, 'w') as myzip:
                 for each_photo in photos_in_directory:
                     each_photo_path = os.path.join(root_path, each_photo)
                     myzip.write(each_photo_path)
             print('Finished creating zip for: ' + root_path)
+    return {'successful_created_zips': True}
 
 
 app.run(host=DEFAULT_HOST, port=DEFAULT_PORT, debug=True)
